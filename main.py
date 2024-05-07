f3bbf78ecb96222844b89e75d42fccc4b72b118e
Nutella-lite <natalysny2012@gmail.com>
2024-05-07 21:16:47 +0300

    add button

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ pygame.init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 pygame.display.set_caption("Игра Тир")
-
-icon = pygame.image.load("img/target.png")
+icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("img/button.png")
+target_width, target_height = 300, 65
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 
 running = True
