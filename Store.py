008715f7807e9faf448e2477ef516b29da798817
Nutella-lite <natalysny2012@gmail.com>
2024-05-13 15:03:47 +0300

    Update Store.py

--- Store.py
+++ Store.py
@@ -4,23 +4,21 @@ class Store:
         self.address = address
         self.items = {}
 
-    def add_item(self, item):
-        self.items.update(item)
+    def update_item(self,  item_name, item_price):
+        self.items[item_name] = item_price
 
-    def remove_item(self, key):
-        self.items.pop(key)
+    def remove_item(self, it_name):
+        self.items.pop(it_name)
 
-    def get_item(self, key):
-        return self.items.get(key, default=None)
+    def get_item_price(self, it_name):
+        return self.items.get(it_name, None)
 
-    def update_value(self, key):
-        self.items.update(key)
 
 class Stores:
     def __init__(self):
         self.stores = []
 
-    def add_store(self, store):
+    def add_store(self):
         n = int(input("Какое количество магазинов хотите добавить: "))
         for i in range(n):
             name = input(f"Введите название магазина: ")
@@ -29,10 +27,82 @@ class Stores:
             self.stores.append(store)
 
     def add_item_to_store(self):
-        store_name = input("Введите название магазина: ")
-        store = self.get_store_by_name(store_name)
-        if store is not None:
-            item_name = input("Введите название товара: ")
-            item = self.get_item_by_name(item_name)
-            if item is not None:
-                store.add_item(item)
+        store_address = input("Введите адрес магазина: ")
+        store_found = False
+        for store in self.stores:
+            if store.address == store_address:
+                store_found = True
+                m = int(input("Какое количество товаров хотите добавить / обновить: "))
+                for i in range(m):
+                    item_name = input("Введите название товара: ")
+                    item_price = float(input("Введите цену товара: "))
+                    store.update_item(item_name, item_price)
+        if not store_found:
+            print("Магазин не найден")
+
+    def remove_item_from_store(self):
+        store_address = input("Введите адрес магазина: ")
+        store_found = False
+        for store in self.stores:
+            if store.address == store_address:
+                store_found = True
+                item_name = input("Введите название товара: ")
+                store.remove_item(item_name)
+        if not store_found:
+            print("Магазин не найден")
+
+    def get_item_range(self):
+        store_address = input("Введите адрес магазина: ")
+        store_found = False
+        for store in self.stores:
+            if store.address == store_address:
+                store_found = True
+                print (store.items)
+        if not store_found:
+            print("Магазин не найден")
+
+
+    def get_item_prices(self):
+        item_name = input("Введите название товара: ")
+        for store in self.stores:
+            print(f"Магазин '{store.name}' по адресу: {store.address}")
+            print(store.get_item_price(item_name))
+
+    def show_stores(self):
+        if len(self.stores) == 0:
+            print("магазинов пока нет")
+            return
+        else:
+            i = 1
+            for store in self.stores:
+                print(f"{i}) '{store.name}' по адресу: {store.address}")
+                i += 1
+
+my_stores = Stores()
+running = True
+while running:
+    action = int(input("Выберите действие: "
+                       "\n1 - показать список магазинов "
+                       "\n2 - добавить товары в магазин "
+                       "\n3 - удалить товары из магазина "
+                       "\n4 - получить цены товара в магазинах "
+                       "\n5 - обновить цену товара в магазине "
+                       "\n6 - получить ассортимент товаров в магазине "
+                       "\n7 - добавить магазин "
+                       "\n8 - выход из программы \n"))
+    if action == 1:
+        my_stores.show_stores()
+    elif action == 2:
+        my_stores.add_item_to_store()
+    elif action == 3:
+        my_stores.remove_item_from_store()
+    elif action == 4:
+        my_stores.get_item_prices()
+    elif action == 5:
+        my_stores.add_item_to_store()
+    elif action == 6:
+        my_stores.get_item_range()
+    elif action == 7:
+        my_stores.add_store()
+    else:
+        running = False
