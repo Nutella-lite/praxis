2f5395315eea8a289129b6b7d048775a797d2528
Nutella-lite <natalysny2012@gmail.com>
2024-07-07 19:10:48 +0300

    create scrapy project

---
+++ divanpars/divanpars/spiders/divan_pars.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class DivanParsSpider(scrapy.Spider):
+    name = "divan_pars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                'name' : divan.css('div.wYUX2 span::text').get(),
+                'price' : divan.css('div.q5Uds span::text').get(),
+                'link' : divan.css('a').attrib['href']
+            }
