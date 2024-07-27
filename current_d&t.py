bcc97585d687721df5417ebb052f80ba1ad459cf
Nutella-lite <natalysny2012@gmail.com>
2024-07-27 13:28:19 +0300

    update current_d&t.py

---
+++ current_d&t.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+
+print(f"Текущая дата и время: {datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def current_time():
+    return f"Текущие дата и время: {datetime.now().strftime('%d.%m.%Y - %H:%M:%S')}"
+
+@app.route("/new/")
+@app.route("/newpage/")
+def new():
+    return "new page"
+
+if __name__ == "__main__":
+    app.run()
