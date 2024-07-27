1b382c150ef6c0afa85eb7fc059dc2a44a05c17a
Nutella-lite <natalysny2012@gmail.com>
2024-07-27 18:02:35 +0300

    add files

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 @app.route("/")
-def current_time():
-    return f"Текущие дата и время: {datetime.now().strftime('%d.%m.%Y - %H:%M:%S')}"
-@app.route("/new/")
-@app.route("/newpage/")
-def new():
-    return "new page"
+def main_page():
+    return render_template("index.html")
+@app.route("/blog/")
+def blog_page():
+    return render_template("blog.html")
+
+@app.route("/contacts/")
+def contacts_page():
+    return render_template("contacts.html")
+
 
 if __name__ == "__main__":
     app.run()
