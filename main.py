13ae4f6ea4ab8fd7bca5571b38d5419fd0056c02
Nutella-lite <natalysny2012@gmail.com>
2024-07-27 13:59:43 +0300

    update paths

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from flask import Flask
 from datetime import datetime
 
 app = Flask(__name__)
-
 @app.route("/")
 def current_time():
     return f"Текущие дата и время: {datetime.now().strftime('%d.%m.%Y - %H:%M:%S')}"
-
 @app.route("/new/")
 @app.route("/newpage/")
 def new():
