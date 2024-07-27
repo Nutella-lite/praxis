10b95d1ecfcbc92ca22408cd6cf6e7c8f3f1a575
Nutella-lite <natalysny2012@gmail.com>
2024-07-27 16:02:15 +0300

    reorder

--- current_d&t.py
+++ current_d&t.py
@@ -1,3 +1,10 @@
+from flask import Flask
 from datetime import datetime
 
-print(f"Текущая дата и время: {datetime.now().strftime('%d.%m.%Y %H:%M:%S')}")
+app = Flask(__name__)
+@app.route("/")
+def current_time():
+    return f"Текущие дата и время: {datetime.now().strftime('%d.%m.%Y - %H:%M:%S')}"
+
+if __name__ == "__main__":
+    app.run()
